5c9635a6fdc477ca2722f82016978393860fdbe4
Mansi <mansigangwani5645@gmail.com>
2022-11-06 10:43:09 +0530

    changing to different branch

--- manage_data/urls.py
+++ manage_data/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/',display_events,name='display_events'),
+    path('',display_events,name='display_events'),
 
     path('display_events',display_events,name='display_events'),
 
